e3be8597bb61f98185ed3a16f58cde7e9ec748b9
Kristopher Cage <kristophercage@gmail.com>
2021-04-19 22:06:54 -0500

    added another algorithm and updated spec to test both solutions

--- ArrayAnalyzer.rb
+++ ArrayAnalyzer.rb
@@ -1,8 +1,7 @@
 class ArrayAnalyzer 
-    def self.findHighestSum(array)
-        if (array.length < 2)
-            raise(ArgumentError, 'Invalid Array! Input array must contain two or more integers')
-        end
+    def self.findHighestSumWithNestedLoops(array)
+        verifyInput(array)
+
         if (array.length == 2)
             return array
         end
@@ -21,5 +20,78 @@ class ArrayAnalyzer
             end
         end 
         return results
+    end
+
+    
+    # We need the two values that provide the highest sum, but they cannot be adjacent.
+    # if we only get the two highest values, they might be adjacent
+    # if we only get the three highest value, then the middle number might be large enough that the outer numbers do not exceed it
+    # we should get the four highest values and store their indices. Then we can find the highest sum of those
+    # that means O(n) to go through the array and find the four highest and O(1) to check the remaining four, and so O(n) total
+    # will need to verify this through testing. Make some large arrays and compare nested v this
+    def self.findHighestSumWithHighestValues(array)
+        verifyInput(array)
+        
+        if (array.length == 2)
+            return array
+        end
+
+        fourHighest = []
+        fourHighest << 0
+        for i in 1...array.length
+            fourHighest = updateFourHighest(array, i, fourHighest)
+        end
+
+        maxSum = -1
+        results = [-1,-1]
+        for i in 0..1
+            for j in i+1...fourHighest.length
+                # if the absolute value of subtracting the indices is one, then these are adjacent
+                if (fourHighest[i] - fourHighest[j]).abs == 1
+                    next
+                end
+                newSum = array[fourHighest[i]] + array[fourHighest[j]]
+                if (newSum > maxSum)
+                    maxSum = newSum
+                    results[0] = array[fourHighest[i]]
+                    results[1] = array[fourHighest[j]]
+                end
+            end
+        end
+        return results
+    end
+
+    def self.updateFourHighest(array, i, fourHighest)
+        # create a new array of the highest values (max of four) between the current array and this new value
+        tempArray = []
+        spaceFound = false
+        for j in 0...fourHighest.length
+            # if the current value from the array is greater than the current fourHighest indicies array value, then we add i to the tempArray
+            if (array[i] >= array[fourHighest[j]])
+                spaceFound = true
+                tempArray << i
+                # then we fill the temp array with the remaining values
+                for k in j...fourHighest.length
+                    tempArray << fourHighest[k]
+                end
+            else
+                tempArray << fourHighest[j]
+            end
+        end
+        if !spaceFound
+            tempArray << i
+        end
+        # keep up to four
+        if (tempArray.length > 4)
+            return tempArray[0..3]
+        else
+            return tempArray.dup
+        end
+    end
+
+    def self.verifyInput(array)
+        if (array.length < 2)
+            raise(ArgumentError, 'Invalid Array! Input array must contain two or more integers')
+        end
     end
 end
--- spec/ArrayAnalyzer_spec.rb
+++ spec/ArrayAnalyzer_spec.rb
@@ -2,81 +2,150 @@ require './ArrayAnalyzer'
 require 'rspec'
 
 RSpec.describe "An ArrayAnalyzer can" do 
-    context 'When given an invalid array, raises argument error' do
-        it 'Given an empty array' do
-            testArray = []
-            expect{ ArrayAnalyzer.findHighestSum(testArray) }
-            .to raise_error('Invalid Array! Input array must contain two or more integers') 
-        end
-        it 'Given an array with only one integer' do
-            testArray = [1]
-            expect{ ArrayAnalyzer.findHighestSum(testArray) }
-            .to raise_error('Invalid Array! Input array must contain two or more integers')
+    context 'General Functionality' do
+        context 'When given an invalid array, raises argument error' do
+            it 'Given an empty array' do
+                testArray = []
+                expect{ ArrayAnalyzer.verifyInput(testArray) }
+                .to raise_error('Invalid Array! Input array must contain two or more integers') 
+            end
+            it 'Given an array with only one integer' do
+                testArray = [1]
+                expect{ ArrayAnalyzer.verifyInput(testArray) }
+                .to raise_error('Invalid Array! Input array must contain two or more integers')
+            end
         end
     end
     context 'Given an array, it can find the two non-adjacent integers with the highest sum' do
-        # Basic case: just two integers
-        it "Given [1,2], expect [1,2]" do
-            testArray = [1,2]
-            expectedResult = [1,2]
-            result = ArrayAnalyzer.findHighestSum(testArray)
-            expect(result).to match(expectedResult)
+        context 'Using Nested Loops' do
+            # Basic case: just two integers
+            it "Given [1,2], expect [1,2]" do
+                testArray = [1,2]
+                expectedResult = [1,2]
+                result = ArrayAnalyzer.findHighestSumWithNestedLoops(testArray)
+                expect(result).to match(expectedResult)
+            end
+            # Basic case: more than two integers, but no duplicates
+            it "Given [2,4,2,3,1], expect [4,3]" do
+                testArray = [2,4,2,3,1]
+                expectedResult = [4,3]
+                result = ArrayAnalyzer.findHighestSumWithNestedLoops(testArray)
+                expect(result).to match(expectedResult)
+            end
+            # Edge case: more than two integers, but duplicates 
+            it "Given [1,10,11,11,12,1], expect [11,12]" do
+                testArray = [1,10,11,11,12,1]
+                expectedResult = [11,12]
+                result = ArrayAnalyzer.findHighestSumWithNestedLoops(testArray)
+                expect(result).to match(expectedResult)
+            end
+            # Edge case: more than two integers, the two largest integers are adjacent
+            it "Given [3,6,5,1,1], expect [3,5]" do
+                testArray = [3,6,5,1,1]
+                expectedResult = [3,5]
+                result = ArrayAnalyzer.findHighestSumWithNestedLoops(testArray)
+                expect(result).to match(expectedResult)
+            end
+            # Edge case: more than two integers, duplicates that are the highest
+            it "Given [1,2,6,6,6,2,1], expect [6,6]" do
+                testArray = [1,2,6,6,6,2,1]
+                expectedResult = [6,6]
+                result = ArrayAnalyzer.findHighestSumWithNestedLoops(testArray)
+                expect(result).to match(expectedResult)
+            end
+            # Edge case: more than two integers, and highest are the first and final integers
+            it "Given [7,1,1,1,1,8], expect [7,8]" do
+                testArray = [7,1,1,1,1,8]
+                expectedResult = [7,8]
+                result = ArrayAnalyzer.findHighestSumWithNestedLoops(testArray)
+                expect(result).to match(expectedResult)
+            end
+            # Edge case: more than two integers, and the highest are the last and second to last
+            it "Given [1,1,1,5,3,8], expect [5,8]" do
+                testArray = [1,1,1,5,3,8]
+                expectedResult = [5,8]
+                result = ArrayAnalyzer.findHighestSumWithNestedLoops(testArray)
+                expect(result).to match(expectedResult)
+            end
+            # Edge case: more than two integers, and the highest are the first and third
+            it "Given [5,2,4,1,1,1], expect [5,4]" do
+                testArray = [5,2,4,1,1,1]
+                expectedResult = [5,4]
+                result = ArrayAnalyzer.findHighestSumWithNestedLoops(testArray)
+                expect(result).to match(expectedResult)
+            end
+            # Edge case: more than two integers, all are zeros
+            it "Given [0,0,0,0,0,0], expect [0,0]" do
+                testArray = [0,0,0,0,0,0]
+                expectedResult = [0,0]
+                result = ArrayAnalyzer.findHighestSumWithNestedLoops(testArray)
+                expect(result).to match(expectedResult)
+            end
         end
-        # Basic case: more than two integers, but no duplicates
-        it "Given [2,4,2,3,1], expect [4,3]" do
-            testArray = [2,4,2,3,1]
-            expectedResult = [4,3]
-            result = ArrayAnalyzer.findHighestSum(testArray)
-            expect(result).to match(expectedResult)
-        end
-        # Edge case: more than two integers, but duplicates 
-        it "Given [1,10,11,11,12,1], expect [11,12]" do
-            testArray = [1,10,11,11,12,1]
-            expectedResult = [11,12]
-            result = ArrayAnalyzer.findHighestSum(testArray)
-            expect(result).to match(expectedResult)
-        end
-        # Edge case: more than two integers, the two largest integers are adjacent
-        it "Given [3,6,5,1,1], expect [3,5]" do
-            testArray = [3,6,5,1,1]
-            expectedResult = [3,5]
-            result = ArrayAnalyzer.findHighestSum(testArray)
-            expect(result).to match(expectedResult)
-        end
-        # Edge case: more than two integers, duplicates that are the highest
-        it "Given [1,2,6,6,6,2,1], expect [6,6]" do
-            testArray = [1,2,6,6,6,2,1]
-            expectedResult = [6,6]
-            result = ArrayAnalyzer.findHighestSum(testArray)
-            expect(result).to match(expectedResult)
-        end
-        # Edge case: more than two integers, and highest are the first and final integers
-        it "Given [7,1,1,1,1,8], expect [7,8]" do
-            testArray = [7,1,1,1,1,8]
-            expectedResult = [7,8]
-            result = ArrayAnalyzer.findHighestSum(testArray)
-            expect(result).to match(expectedResult)
-        end
-        # Edge case: more than two integers, and the highest are the last and second to last
-        it "Given [1,1,1,5,3,8], expect [5,8]" do
-            testArray = [1,1,1,5,3,8]
-            expectedResult = [5,8]
-            result = ArrayAnalyzer.findHighestSum(testArray)
-            expect(result).to match(expectedResult)
-        end
-        # Edge case: more than two integers, and the highest are the first and third
-        it "Given [5,2,4,1,1,1], expect [5,4]" do
-            testArray = [5,2,4,1,1,1]
-            expectedResult = [5,4]
-            result = ArrayAnalyzer.findHighestSum(testArray)
-            expect(result).to match(expectedResult)
-        end
-        # Edge case: more than two integers, all are zeros
-        it "Given [0,0,0,0,0,0], expect [0,0]" do
-            testArray = [0,0,0,0,0,0]
-            expectedResult = [0,0]
-            result = ArrayAnalyzer.findHighestSum(testArray)
-            expect(result).to match(expectedResult)
+        context 'Using highest values' do
+            # Basic case: just two integers
+            it "Given [1,2], expect [1,2]" do
+                testArray = [1,2]
+                expectedResult = [1,2]
+                result = ArrayAnalyzer.findHighestSumWithHighestValues(testArray)
+                expect(result).to match(expectedResult)
+            end
+            # Basic case: more than two integers, but no duplicates
+            it "Given [2,4,2,3,1], expect [4,3]" do
+                testArray = [2,4,2,3,1]
+                expectedResult = [4,3]
+                result = ArrayAnalyzer.findHighestSumWithHighestValues(testArray)
+                expect(result).to match(expectedResult)
+            end
+            # Edge case: more than two integers, but duplicates 
+            it "Given [1,10,11,11,12,1], expect [12,11]" do
+                testArray = [1,10,11,11,12,1]
+                expectedResult = [12,11]
+                result = ArrayAnalyzer.findHighestSumWithHighestValues(testArray)
+                expect(result).to match(expectedResult)
+            end
+            # Edge case: more than two integers, the two largest integers are adjacent
+            it "Given [3,6,5,1,1], expect [5,3]" do
+                testArray = [3,6,5,1,1]
+                expectedResult = [5,3]
+                result = ArrayAnalyzer.findHighestSumWithHighestValues(testArray)
+                expect(result).to match(expectedResult)
+            end
+            # Edge case: more than two integers, duplicates that are the highest
+            it "Given [1,2,6,6,6,2,1], expect [6,6]" do
+                testArray = [1,2,6,6,6,2,1]
+                expectedResult = [6,6]
+                result = ArrayAnalyzer.findHighestSumWithHighestValues(testArray)
+                expect(result).to match(expectedResult)
+            end
+            # Edge case: more than two integers, and highest are the first and final integers
+            it "Given [7,1,1,1,1,8], expect [8,7]" do
+                testArray = [7,1,1,1,1,8]
+                expectedResult = [8,7]
+                result = ArrayAnalyzer.findHighestSumWithHighestValues(testArray)
+                expect(result).to match(expectedResult)
+            end
+            # Edge case: more than two integers, and the highest are the last and second to last
+            it "Given [1,1,1,5,3,8], expect [8,5]" do
+                testArray = [1,1,1,5,3,8]
+                expectedResult = [8,5]
+                result = ArrayAnalyzer.findHighestSumWithHighestValues(testArray)
+                expect(result).to match(expectedResult)
+            end
+            # Edge case: more than two integers, and the highest are the first and third
+            it "Given [5,2,4,1,1,1], expect [5,4]" do
+                testArray = [5,2,4,1,1,1]
+                expectedResult = [5,4]
+                result = ArrayAnalyzer.findHighestSumWithHighestValues(testArray)
+                expect(result).to match(expectedResult)
+            end
+            # Edge case: more than two integers, all are zeros
+            it "Given [0,0,0,0,0,0], expect [0,0]" do
+                testArray = [0,0,0,0,0,0]
+                expectedResult = [0,0]
+                result = ArrayAnalyzer.findHighestSumWithHighestValues(testArray)
+                expect(result).to match(expectedResult)
+            end
         end
     end
 end
